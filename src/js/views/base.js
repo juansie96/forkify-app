67eda996b9f7858c35af6f109ffe7264007012f7
Pulga <juansie.96@gmail.com>
2020-04-02 10:34:19 -0300

    Build the Shopping List View

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,6 +5,7 @@ export const elements = {
     searchResList: document.querySelector('.results__list'),
     searchResPages: document.querySelector('.results__pages'),
     recipe: document.querySelector('.recipe'),
+    shoppingList: document.querySelectorAll('.shopping__list'),
     loader: document.querySelector('.Loader')
 }
 
